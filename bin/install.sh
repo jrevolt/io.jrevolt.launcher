63085f05cc6af4e92096cb0060be64a7d69e83c6
Patrik Beno <patrik.beno@greenhorn.sk>
2015-09-08 01:21:57 +0200

    scripts

---
+++ bin/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+case $(whoami) in
+	root)	dst="/etc/springboot" ;;
+	*)	dst="$HOME/.springboot" ;;
+esac
+
+echo "Creating $dst directory..."
+mkdir -p $dst
+
+repo="https://raw.githubusercontent.com/jrevolt/io.jrevolt.launcher"
+branch="develop"
+
+echo "Downloading updater..."
+wget -q -O $dst/update.sh $repo/$branch/update.sh
+
+echo "Updating launcher..."
+chmod a+x $dst/update.sh
+$dst/update.sh
+
+echo "Downloading launcher script..."
+wget -q -O $dst/springboot.sh $repo/$branch/springboot.sh
+chmod a+x $dst/springboot.sh
+
+if [ -d ~/bin ]; then
+	[ -L ~/bin/springboot ] || ln -s ~/.springboot/springboot.sh ~/bin/springboot
+	ls -l ~/bin/springboot
+fi
---
+++ bin/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+grepo="jrevolt/io.jrevot.launcher"
+gbranch="develop"
+
+repo="${1:-http://repo.jrevolt.io}"
+groupid="io.jrevolt.launcher"
+artifactid="io.jrevolt.launcher"
+version="${2:-develop-SNAPSHOT}"
+
+url="${repo}/service/local/artifact/maven/redirect?r=snapshots&g=${groupid}&a=${artifactid}&v=${version}&e=jar"
+
+echo "Updating JRevolt Launcher..."
+cd $(dirname $0)
+
+url=$(curl -sk --head "$url" \
+	| perl -n -e '/^Location: (.*)$/ && print "$1\n"' \
+	| sed 's/\r//'
+)
+curl -sk --remote-name $url
+[ -L springboot.jar ] && rm springboot.jar
+ln -s $(ls -1 spring-boot-launcher-*.jar | tail -n1) springboot.jar
+ls -l springboot.jar
+chmod 640 *.jar
+
+wget -q -N https://raw.githubusercontent.com/${grepo}/${gbranch}/springboot.sh
+chmod a+x springboot.sh
