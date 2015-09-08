8b636aa1d59f6d530a2e5888597f8c98c3daf906
Patrik Beno <patrik.beno@greenhorn.sk>
2015-09-08 02:19:09 +0200

    scripts

--- bin/update.sh
+++ bin/update.sh
@@ -17,20 +17,22 @@ groupid="io.jrevolt.launcher"
 artifactid="io.jrevolt.launcher"
 version="${2:-develop-SNAPSHOT}"
 
-url="${repo}/service/local/artifact/maven/redirect?r=snapshots&g=${groupid}&a=${artifactid}&v=${version}&e=jar"
+urljar="${repo}/service/local/artifact/maven/redirect?r=snapshots&g=${groupid}&a=${artifactid}&v=${version}&e=jar"
+urlsh="https://raw.githubusercontent.com/${grepo}/${gbranch}/bin/jrevolt.sh"
+fjar="io.jrevolt.launcher.jar"
 
 echo "Updating JRevolt Launcher..."
 cd $(dirname $0)
 
-url=$(curl -sk --head "$url" \
+url=$(curl -sk --head "$urljar" \
 	| perl -n -e '/^Location: (.*)$/ && print "$1\n"' \
 	| sed 's/\r//'
 )
 curl -sk --remote-name $url
-[ -L io.jrevolt.launcher.jar ] && rm io.jrevolt.launcher.jar
-ln -s $(ls -1 io.jrevolt.launcher-*.jar | tail -n1) io.jrevolt.launcher.jar
-ls -l io.jrevolt.launcher.jar
+[ -L $fjar ] && rm $fjar
+ln -s $(ls -1 io.jrevolt.launcher-*.jar | tail -n1) $fjar
+ls -l $fjar
 chmod 640 *.jar
 
-wget -q -N https://raw.githubusercontent.com/${grepo}/${gbranch}/bin/jrevolt.sh
-chmod a+x jrevolt.sh
+wget -q -N $urlsh
+chmod a+x $(basename $urlsh)
