8bd9c0ec1f8d22108c81c9c63b722e74b0fb73db
patrikbeno <patrikbeno@gmail.com>
2015-09-08 16:03:18 +0200

    Update jrevolt.sh

--- bin/jrevolt.sh
+++ bin/jrevolt.sh
@@ -6,4 +6,4 @@ java="${JAVA_HOME:-/usr/java/default}/bin/java"
 
 file=~/.jrevolt/io.jrevolt.launcher.jar
 file=$(which cygpath > /dev/null 2>&1 && cygpath -w $file || realpath $file)
-java ${JVM_OPTIONS:-} -jar $file ${SPRINGBOOT_LAUNCHER_OPTIONS:-} $*
+$java ${JVM_OPTIONS:-} -jar $file ${SPRINGBOOT_LAUNCHER_OPTIONS:-} $*
