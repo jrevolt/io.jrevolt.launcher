6038edfa21ba7cb98ba6680e246324229bf5ec0b
Patrik Beno <patrik.beno@greenhorn.sk>
2015-10-05 02:57:32 +0200

    scripts

--- bin/jrevolt.sh
+++ bin/jrevolt.sh
@@ -5,6 +5,9 @@ realpath() {
 	echo "$(cd $(dirname $1) && pwd -L)/$(basename $1)"
 }
 
-file=~/.jrevolt/io.jrevolt.launcher.jar
-file=$(which cygpath > /dev/null 2>&1 && cygpath -w $file || $(realpath $file))
-java ${JVM_OPTIONS:-} -jar $file ${SPRINGBOOT_LAUNCHER_OPTIONS:-} $*
+java=$(which java)
+jvmoptions="${JVM_OPTIONS:-}"
+file=$(realpath ~/.jrevolt/io.jrevolt.launcher.jar)
+launcheropts="${JREVOLT_LAUNCHER_OTPIONS:-}"
+
+$java $jvmoptions -jar $file $launcheropts $@
--- bin/update.sh
+++ bin/update.sh
@@ -7,15 +7,20 @@ error() {
 	exit
 }
 
+realpath() {
+	echo "$(cd $(dirname $1) && pwd -L)/$(basename $1)"
+}
+
 trap 'error ${LINENO}' ERR
+
+repo="${1:-http://repo.jrevolt.io}"
+version="${2:-develop-SNAPSHOT}"
 
 grepo="jrevolt/io.jrevolt.launcher"
 gbranch="develop"
 
-repo="${1:-http://repo.jrevolt.io}"
 groupid="io.jrevolt.launcher"
 artifactid="io.jrevolt.launcher"
-version="${2:-develop-SNAPSHOT}"
 
 urljar="${repo}/service/local/artifact/maven/redirect?r=snapshots&g=${groupid}&a=${artifactid}&v=${version}&e=jar"
 urlsh="https://raw.githubusercontent.com/${grepo}/${gbranch}/bin/jrevolt.sh"
@@ -25,12 +30,14 @@ echo "Updating JRevolt Launcher..."
 cd $(dirname $0)
 
 url=$(curl -sk --head "$urljar" | grep "Location:" | sed "s/Location: //" | tr -d '\r')
-curl -sk --remote-name $url
+
+wget -qN $url
+
 [ -L $fjar ] && rm $fjar
 ln -s $(ls -1 io.jrevolt.launcher-*.jar | tail -n1) $fjar
 chmod 640 $fjar
 
-wget -q -N $urlsh
+wget -qN $urlsh
 chmod a+x $(basename $urlsh)
 
 ls -l $fjar $(basename $urlsh)
