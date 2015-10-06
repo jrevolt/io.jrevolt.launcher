a652f399c1b08b58ad095becd833ce74c40021d8
patrikbeno <patrikbeno@gmail.com>
2015-10-07 00:31:14 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,41 +8,48 @@ realpath() {
 	echo "$(cd $(dirname $1) && pwd -L)/$(basename $1)"
 }
 
-echo "Updating JRevolt Launcher scripts..."
-git pull
+update1() {
+	echo "Updating JRevolt Launcher scripts..."
+	git pull
+	$0 update2
+}
 
-dflt=master
-[ -f .version ] && dflt="$(cat .version)"
+update2() {
+	dflt=master
+	[ -f .version ] && dflt="$(cat .version)"
+	
+	# github
+	grepo="jrevolt/io.jrevolt.launcher"
+	gversion="${1:-$dflt}"
+	
+	if [[ $gversion =~ "^tag:.*" ]]; then
+		gversion="${gversion//tag:}"
+		mversion=$gversion
+		reponame=releases
+	else
+		mversion="${gversion//*\/}-SNAPSHOT"
+		reponame=snapshots
+	fi
+	
+	# maven
+	repo="${2:-http://repo.jrevolt.io}"
+	groupid="io.jrevolt.launcher"
+	artifactid="io.jrevolt.launcher"
+	
+	urljar="${repo}/service/local/artifact/maven/redirect?r=${reponame}&g=${groupid}&a=${artifactid}&v=${mversion}&e=jar"
+	fjar="io.jrevolt.launcher.jar"
+	
+	echo "Updating JRevolt Launcher library..."
+	cd $(dirname $0)
+	
+	url=$(curl -sk --head "$urljar" | grep "Location:" | sed "s/Location: //" | tr -d '\r')
+	
+	wget -qN $url
+	
+	[ -L $fjar ] && rm $fjar
+	ln -s $(basename $url) $fjar
+	chmod 640 $fjar
+	echo "$(basename $url)"
+}
 
-# github
-grepo="jrevolt/io.jrevolt.launcher"
-gversion="${1:-$dflt}"
-
-if [[ $gversion =~ "^tag:.*" ]]; then
-	gversion="${gversion//tag:}"
-	mversion=$gversion
-	reponame=releases
-else
-	mversion="${gversion//*\/}-SNAPSHOT"
-	reponame=snapshots
-fi
-
-# maven
-repo="${2:-http://repo.jrevolt.io}"
-groupid="io.jrevolt.launcher"
-artifactid="io.jrevolt.launcher"
-
-urljar="${repo}/service/local/artifact/maven/redirect?r=${reponame}&g=${groupid}&a=${artifactid}&v=${mversion}&e=jar"
-fjar="io.jrevolt.launcher.jar"
-
-echo "Updating JRevolt Launcher library..."
-cd $(dirname $0)
-
-url=$(curl -sk --head "$urljar" | grep "Location:" | sed "s/Location: //" | tr -d '\r')
-
-wget -qN $url
-
-[ -L $fjar ] && rm $fjar
-ln -s $(basename $url) $fjar
-chmod 640 $fjar
-echo "$(basename $url)"
+${@:-update1}
